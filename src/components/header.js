3a99a5191ce05be51a769b610e582d07f4cafc2b
danhamraj <danhamraj@gmail.com>
2019-11-06 17:09:40 -0500

    Created components folder and added header, paragraph, image.js added styles to index.js

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = props => {
+
+return (
+
+<div>
+
+<h1 className = "title" > {props.title} </h1>
+
+<p className = "date">
+
+Date: <span> {props.date} </span>
+
+</p>
+
+</div>
+
+);
+
+};
+
+
+
+export default Header;
---
+++ src/components/image.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Image = props => {
+
+return <img className = "image"src = {props.src} alt = {props.note}/>;
+
+};
+
+export default Image;
